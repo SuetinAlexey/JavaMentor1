6ac168f83bcb08653200e0282f943db976e88c80
Alexey <avsuetin@gmail.com>
2021-01-08 09:22:30 +0300

    добавил HW_3_GarbageCollector

---
+++ HW_3_GarbageCollector/src/main/java/pkghw/App.java
@@ -0,0 +1,13 @@
+package pkghw;
+
+/**
+ * Test HW_3_GarbageCollector!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Garbage Collector" );
+    }
+}
