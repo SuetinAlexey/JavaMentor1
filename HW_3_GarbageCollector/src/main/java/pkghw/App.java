f5769442fc9289537253dbef9ed81b1f5e0369fb
Alexey <avsuetin@gmail.com>
2021-01-08 16:09:37 +0300

    test1

--- HW_3_GarbageCollector/src/main/java/pkghw/App.java
+++ HW_3_GarbageCollector/src/main/java/pkghw/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Garbage Collector!!!" );
+        System.out.println( "Hello World! Garbage Collector_1" );
     }
 }
