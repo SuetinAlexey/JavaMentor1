8bcc6d7936c88cf174efea087325ef7ed6095baf
Alexey <avsuetin@gmail.com>
2021-01-14 13:57:57 +0300

    Изучаем generics.

--- Generics/src/main/java/pkghw/GenericsBounded.java
+++ Generics/src/main/java/pkghw/GenericsBounded.java
@@ -1,5 +1,30 @@
 package pkghw;
 
-public class GenericsBounded {
+import bounds.Cat;
+import bounds.HomeCat;
+import bounds.WildCat;
+
+public class GenericsBounded<T extends Cat> {
+
+    public static void main(String[] args) {
+        //
+        GenericsBounded<Cat> cat = new GenericsBounded<>();
+        GenericsBounded<HomeCat> hc = new GenericsBounded<>();
+        GenericsBounded<WildCat> mc = new GenericsBounded<>();
+        //
+        cat.add( new Cat());
+        cat.add( new HomeCat("Barsik"));
+        cat.add( new WildCat("Lion"));
+        //
+        hc.add( new HomeCat("Barsik"));
+        //hc.add( new Cat());
+
+
+    }
+
+    public void add(T item) {
+        //
+    }
+
 
 }
---
+++ Generics/src/main/java/pkghw/GenericsInheritance.java
@@ -0,0 +1,23 @@
+package pkghw;
+
+import bounds.Animal;
+import bounds.Cat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsInheritance {
+
+    public static void main(String[] args){
+        //
+        Cat cat = new Cat();
+        //
+        List<Cat> catlist = new ArrayList<>();
+
+        catlist.add(cat);
+
+
+
+    }
+
+}
